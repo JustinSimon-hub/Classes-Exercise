8fcabc9ab9c3d46e27ebba843d7eedd0975fded2
Justin Simon <justinsimoncs@gmail.com>
2025-11-13 09:40:54 -0800

    Added main project files

---
+++ Classes/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Classes
+{ 
+    /// <summary>
+    /// Summary description for Car
+    /// </summary>
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public Car()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+    }
+}
